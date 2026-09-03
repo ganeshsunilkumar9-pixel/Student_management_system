a593a154a88b4f37a8bb650b55209efb6b580c24
Ganesh sunil kumar <ganeshsunilkumar9@gmail.com>
2026-09-03 07:01:07 +0000

    project

--- students/models.py
+++ students/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    code = models.CharField(max_length=100,unique=True)
+    hod_name =  models.CharField(max_length=100,blank=True)
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100,unique=True)
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE
+        related_name='course',
+    )
+    semester = models.SmallIntegerField(default=1)
+    credits = models.SmallIntegerField(default=1)
